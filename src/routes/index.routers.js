3f31a968b574a9f55055750cfce75896bd83a585
theomelgar <theomelgar@gmail.com>
2023-02-12 22:31:59 -0300

    Test: just testing progress

--- src/routes/index.routers.js
+++ src/routes/index.routers.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import games from './GamesRoutes.js';
 import customers from './CustomersRoutes.js';
-import rentals from './ReantalsRoutes.js';
+// import rentals from './ReantalsRoutes.js';
 
 const router = Router();
 
 router.use('/games', games);
 router.use('/customers', customers);
-router.use('/rentals', rentals);
+// router.use('/rentals', rentals);
 
 export default router;
---
+++ src/schemas/rental.schema.js
@@ -0,0 +1,12 @@
+// import Joi from "joi";
+
+// export default rentalSchema = Joi.object({
+//     customerId: Joi.number().required(),
+//     gameId: Joi.number().required(),
+//     rentDate: Joi.date().required(),    // data em que o aluguel foi feito
+//     daysRented: Joi.number().required(),             // por quantos dias o cliente agendou o aluguel
+//     returnDate: Joi.date(),          // data que o cliente devolveu o jogo (null enquanto não devolvido)
+//     originalPrice: Joi.number().required(),       // preço total do aluguel em centavos (dias alugados vezes o preço por dia do jogo)
+//     delayFee: Joi.number()             // multa total paga por atraso (dias que passaram do prazo vezes o preço por dia do jogo)
+// }) 
+
